e7e5b498039ef0c7df7e7f28ea32b031a0ce4ecb
otaviovale13 <otaviovale13@gmail.com>
2025-06-27 13:34:26 -0300

    Adicionar arquivos de projeto.

---
+++ mix-burguer-full/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mix_burguer_full.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
